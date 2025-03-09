60e36eee0ad73284c4c595b5ca88933d1a350f99
Guilherme Freitas <guilhermeaqf@gmail.com>
2025-03-09 00:53:07 -0300

    feat: add navbar and hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css";
+
+const hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Olá, Sou Guilherme</h1>
+        <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. A quo quas accusantium sed ratione. Harum quibusdam accusamus veritatis maiores nobis tenetur provident nisi ad recusandae! Inventore voluptas id vero molestias?</p>
+        <a href="mailto:guilhermeaqf@gmail.com" className={styles.contactBtn}>Contate-me</a>
+      </div>
+      <img src={getImageUrl("hero/heroImage.png")} alt="" className={styles.heroImg}/>
+      <div className={styles.topBlur}></div>
+      <div className={styles.bottomBlur}></div>
+      
+
+    </section>
+  );
+};
+
+export default hero;
