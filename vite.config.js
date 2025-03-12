de76eef982c2ceb2616ef672d1229d5124505a6b
Guilherme Freitas <guilhermeaqf@gmail.com>
2025-03-12 16:05:52 -0300

    pasta public criada

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,19 @@ export default defineConfig({
       localsConvention: 'camelCase'
     },
   },
+  base: './',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+      },
+    },
+  },
 });
